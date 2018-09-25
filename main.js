13fc25e690f365b137bf9fabc7e82258219ccd6d
wesiang <weesiang@outlook.com>
2018-09-25 17:28:44 +0800

    /films and /films/id endpoints can return json

---
+++ main.js
@@ -0,0 +1,89 @@
+require('dotenv').config()
+
+//Load libraries
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+
+//create an instance of express
+const app=express();
+
+const sqlFindAllFilms = "SELECT * FROM film";
+
+const sqlFindOneFilm = "SELECT film_id, title FROM film WHERE film_id=?";
+
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: process.env.DB_CONLIMIT
+})
+
+
+var makeQuery = (sql, pool)=>{
+    console.log(sql);
+//    console.log(pool);
+
+    return  (args)=>{
+        let queryPromise = new Promise((resolve,reject)=>{
+            pool.getConnection((err,connection)=>{
+                if(err){
+                    reject(err);
+                    return;
+                }
+                console.log("args>>> ", args);
+                connection.query(sql, args || {}, (err,results)=>{
+                    connection.release();
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(results);
+                })
+            });
+
+        });
+        return queryPromise;
+
+    }
+}
+
+var findAllFilms = makeQuery(sqlFindAllFilms, pool);
+var findOneFilmById = makeQuery(sqlFindOneFilm, pool);
+
+//create routes
+app.get('/films',(req,res)=>{
+
+    findAllFilms().then((results)=>{
+        res.json(results);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json(error);
+    });
+
+});
+
+app.get("/films/:filmId", (req, res)=>{
+    console.log("/film params !");
+    let filmId = req.params.filmId;
+    console.log(filmId);
+    findOneFilmById([parseInt(filmId)]).then((results)=>{
+        console.log(results);
+        res.json(results);
+    }).catch((error)=>{
+        res.status(500).json(error);
+    })
+    
+})
+
+
+//Start web server
+//start server on port 3000 if undefined on command line
+const PORT=parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
+
+app.listen(PORT, ()=>{
+    console.info(`Application started on port ${PORT} at ${new Date()}`);
+});
